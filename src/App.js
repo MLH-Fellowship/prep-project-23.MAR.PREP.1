123c69cf33cadbe11d4f41222173690c08a71330
Ahmed Mohamed <ahmedmohamed@Ahmeds-MacBook-Pro.local>
2023-03-14 13:33:15 -0400

    fix api response data rendering bug and add response status check to forecast api call

--- src/App.js
+++ src/App.js
@@ -11,80 +11,89 @@ function App() {
 
   useEffect(() => {
     if (dateTime !== "") {
-       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&dt=${dateTime}&appid=${process.env.REACT_APP_APIKEY}`)
-        .then(res => res.json())
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&dt=${dateTime}&appid=${process.env.REACT_APP_APIKEY}`
+      )
+        .then((res) => res.json())
         .then(
           (result) => {
-            setIsLoaded(true);
-            setResults(result);
-          },
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        )
-    } else {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_APIKEY}`)
-        .then(res => res.json())
-        .then(
-          (result) => {
-            if (result['cod'] !== 200) {
-              setIsLoaded(false)
+            console.log(result);
+            if (result["cod"] !== 200) {
+              setIsLoaded(false);
             } else {
-              setIsLoaded(true);
               setResults(result);
+              setIsLoaded(true);
             }
           },
           (error) => {
             setIsLoaded(true);
             setError(error);
           }
-        )
+        );
+    } else {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_APIKEY}`
+      )
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            console.log(result);
+            if (result["cod"] !== 200) {
+              setIsLoaded(false);
+            } else {
+              setResults(result);
+              setIsLoaded(true);
+            }
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        );
     }
-  }, [city, dateTime])
+  }, [city, dateTime]);
 
   if (error) {
     return <div>Error: {error.message}</div>;
   } else {
-    return <>
-      <img className="logo" src={logo} alt="MLH Prep Logo"></img>
-      <div>
-        <h2>Enter a city below 👇</h2>
-        <input
-          type="text"
-          value={city}
-          onChange={event => setCity(event.target.value)} />
-        <h2>Select a date and time </h2>
-        <input
-          type="datetime-local"
-          value={dateTime}
-          onChange={event => setDateTime(event.target.value)}
-        />
-        <div className="Results">
-          {!isLoaded && <h2>Loading...</h2>}
-          {isLoaded && results && results.list && (
-            <>
-                {results.list.map(item => {
-                  if (item.dt_txt === dateTime) {
-                    return (
-                      <div key={item.dt}>
-                        <h3>{item.weather[0].main}</h3>
-                        <p>Feels like {item.main.feels_like}°C</p>
-                        <i><p>{results.city.name}, {results.city.country}</p></i>
-                      </div>
-                    );
-                  } else {
-                    return null;
-                  }
-                })}
-            </>
-          )}
-          {isLoaded && !results && (
-            <h2>No results found for {city} at {dateTime}</h2>
-          )}
+    return (
+      <>
+        <img className="logo" src={logo} alt="MLH Prep Logo"></img>
+        <div>
+          <h2>Enter a city below 👇</h2>
+          <input
+            type="text"
+            value={city}
+            onChange={(event) => setCity(event.target.value)}
+          />
+          <h2>Select a date and time </h2>
+          <input
+            type="datetime-local"
+            value={dateTime}
+            onChange={(event) => setDateTime(event.target.value)}
+          />
+          <div className="Results">
+            {!isLoaded && <h2>Loading...</h2>}
+            {isLoaded && results && (
+              <>
+                <h3>{results.weather[0].main}</h3>
+                <p>Feels like {results.main.feels_like}°C</p>
+                <i>
+                  <p>
+                    {results.name}, {results.sys.country}
+                  </p>
+                </i>
+              </>
+            )}
+            {isLoaded && !results && (
+              <h2>
+                No results found for {city} at {dateTime}
+              </h2>
+            )}
+          </div>
         </div>
-      </div>
-    </>
+      </>
+    );
   }
 }
 
