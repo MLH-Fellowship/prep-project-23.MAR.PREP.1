b69bfe59aff09ab83832bd2c391d9602157867ee
J0SAL <joyalmeida321@gmail.com>
2023-03-14 22:48:05 +0530

    added map component

---
+++ src/components/map/Map.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+function Map() {
+    return (
+        <div>
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    )
+}
+
+export default Map
