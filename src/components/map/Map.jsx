f006e4dd2717e7c3afdd5a66889211992a8c4034
J0SAL <joyalmeida321@gmail.com>
2023-03-14 23:22:48 +0530

    added css

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import React from "react";
+import leaflet from "leaflet";
+import {
+    MapContainer,
+    TileLayer,
+    Marker,
+    Popup,
+    useMapEvent,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 function Map() {
+    const position = [51.505, -0.09]
     return (
         <div>
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false}
+                style={{
+                    height: "100%",
+                    width: "100%",
+                    borderRadius: "1rem",
+                    minHeight: "300px",
+
+                }}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
+                <Marker position={position}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
