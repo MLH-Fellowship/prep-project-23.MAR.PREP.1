c38faf9e5168a4717d1c13d479564fe0fefb7608
Samyuktha Gopalsamy <samyuktha.gopalsamy@rutgers.edu>
2023-03-13 23:35:19 -0400

    Add date and time

--- src/App.js
+++ src/App.js
@@ -6,26 +6,42 @@ function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [city, setCity] = useState("New York City")
+  const [dateTime, setDateTime] = useState("");
   const [results, setResults] = useState(null);
 
   useEffect(() => {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          if (result['cod'] !== 200) {
-            setIsLoaded(false)
-          } else {
+    if (dateTime !== "") {
+      fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&dt=${dateTime}&appid=${process.env.REACT_APP_APIKEY}`)
+        .then(res => res.json())
+        .then(
+          (result) => {
             setIsLoaded(true);
             setResults(result);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
           }
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [city])
+        )
+    } else {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_APIKEY}`)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            if (result['cod'] !== 200) {
+              setIsLoaded(false)
+            } else {
+              setIsLoaded(true);
+              setResults(result);
+            }
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }
+  }, [city, dateTime])
 
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -38,14 +54,32 @@ function App() {
           type="text"
           value={city}
           onChange={event => setCity(event.target.value)} />
+        <h2>Select a date and time:</h2>
+        <input
+          type="datetime-local"
+          value={dateTime}
+          onChange={event => setDateTime(event.target.value)}
+        />
         <div className="Results">
           {!isLoaded && <h2>Loading...</h2>}
-          {console.log(results)}
-          {isLoaded && results && <>
-            <h3>{results.weather[0].main}</h3>
-            <p>Feels like {results.main.feels_like}°C</p>
-            <i><p>{results.name}, {results.sys.country}</p></i>
-          </>}
+          {isLoaded && results && (
+            <>
+              {results.list.map(item => {
+                if (item.dt_txt === dateTime) {
+                  return (
+                    <div key={item.dt}>
+                      <h3>{item.weather[0].main}</h3>
+                      <p>Feels like {item.main.feels_like}°C</p>
+                      <i><p>{results.city.name}, {results.city.country}</p></i>
+                    </div>
+                  );
+                }
+              })}
+            </>
+          )}
+          {isLoaded && !results && (
+            <h2>No results found for {city} at {dateTime}</h2>
+          )}
         </div>
       </div>
     </>
