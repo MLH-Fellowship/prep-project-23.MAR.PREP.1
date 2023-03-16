67ea3b975398f2de8890c73cfa7e92b36b29625d
Arya-Gupta <arya.gupta99999@gmail.com>
2023-03-16 22:58:26 +0530

    fix: map styling

--- src/components/map/LocationMarker.jsx
+++ src/components/map/LocationMarker.jsx
@@ -5,38 +5,43 @@ import leaflet from "leaflet";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
 const DefaultIcon = leaflet.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow,
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [0, -41],
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [0, -41],
 });
 
 leaflet.Marker.prototype.options.icon = DefaultIcon;
 
-function LocationMarker({ city, position, handleCityChange, handlePositionChange }) {
-
-    const map = useMapEvent("click", async (e) => {
-        if (!e.latlng)
-            return;
-        let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${e.latlng.lat}&lon=${e.latlng.lng}&appid=${process.env.REACT_APP_APIKEY}`)
-
-        res = await res.json()
-
-        handleCityChange(res.name);
-        handlePositionChange(e.latlng.lat, e.latlng.lng);
-        map.flyTo(e.latlng, map.getZoom());
-    });
-
-    useEffect(() => {
-        map.flyTo(position, map.getZoom());
-    }, [position]);
-
-    return position === null ? null : (
-        <Marker position={position}>
-            <Popup>You are at {city}</Popup>
-        </Marker>
+function LocationMarker({
+  city,
+  position,
+  handleCityChange,
+  handlePositionChange,
+}) {
+  const map = useMapEvent("click", async (e) => {
+    if (!e.latlng) return;
+    let res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${e.latlng.lat}&lon=${e.latlng.lng}&appid=${process.env.REACT_APP_APIKEY}`
     );
+
+    res = await res.json();
+
+    handleCityChange(res.name);
+    handlePositionChange(e.latlng.lat, e.latlng.lng);
+    map.flyTo(e.latlng, map.getZoom());
+  });
+
+  useEffect(() => {
+    map.flyTo(position, map.getZoom());
+  }, [position]);
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are at {city}</Popup>
+    </Marker>
+  );
 }
 
 export default LocationMarker;
--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,56 +1,63 @@
 import { useEffect, useState } from "react";
 
-import {
-    MapContainer,
-    TileLayer,
-} from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import LocationMarker from "./LocationMarker";
 
 function Map({ city, handleCityChange }) {
-    const [position, setPosition] = useState([51.505, -0.09]);
+  const [position, setPosition] = useState([51.505, -0.09]);
 
-    const handlePositionChange = (lat, lon) => {
-        setPosition([lat, lon]);
+  const handlePositionChange = (lat, lon) => {
+    setPosition([lat, lon]);
+  };
+
+  const getLocation = async (city) => {
+    let locationData = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${process.env.REACT_APP_APIKEY}`
+    );
+
+    locationData = await locationData.json();
+    if (locationData && locationData.length !== 0) {
+      let lat = locationData[0]?.lat;
+      let lon = locationData[0]?.lon;
+
+      handlePositionChange(lat, lon);
     }
+  };
 
-    const getLocation = async (city) => {
+  useEffect(() => {
+    if (city) getLocation(city);
+  }, [city]);
 
-        let locationData = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${process.env.REACT_APP_APIKEY}`);
-
-        locationData = await locationData.json();
-        if (locationData && locationData.length !== 0) {
-            let lat = locationData[0]?.lat;
-            let lon = locationData[0]?.lon;
-
-            handlePositionChange(lat, lon);
-        }
-    }
-
-
-
-    useEffect(() => {
-        if (city)
-            getLocation(city);
-    }, [city]);
-
-    return (
-        <div>
-            <MapContainer center={position} zoom={13} scrollWheelZoom={false}
-                style={{
-                    height: "90vh",
-                    width: "100%",
-                    borderRadius: "1rem",
-                    minHeight: "300px",
-                }}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <LocationMarker city={city} position={position} handleCityChange={handleCityChange} handlePositionChange={handlePositionChange} />
-            </MapContainer>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{
+          width: "50vw",
+          minHeight: "300px",
+        }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <LocationMarker
+          city={city}
+          position={position}
+          handleCityChange={handleCityChange}
+          handlePositionChange={handlePositionChange}
+        />
+      </MapContainer>
+    </div>
+  );
 }
 
-export default Map
+export default Map;
