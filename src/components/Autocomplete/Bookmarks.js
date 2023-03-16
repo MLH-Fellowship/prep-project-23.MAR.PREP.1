b1840b180e69c5931c164f18881d270184b78353
“Koichi <koichidminami@gmail.com>
2023-03-16 18:02:18 -0400

    added bookmark icon feature

---
+++ src/components/Autocomplete/Bookmarks.js
@@ -0,0 +1,61 @@
+import { Bookmark, BookmarkBorderOutlined } from "@material-ui/icons";
+import { useEffect, useState } from "react";
+
+
+
+const Bookmarks = ({results}) =>{
+    const [fillbookmark, setFillbookmark] = useState(false)
+    
+    useEffect(() =>{
+        // first set Fillbookmark to false and then find out if the city is in the localStorage
+        setFillbookmark(false)
+        const getArrayCities = localStorage.getItem("cities")
+        let addCitiesInArray = JSON.parse(getArrayCities)
+        for( let i in addCitiesInArray){
+            if (addCitiesInArray[i].name === results.name){
+                setFillbookmark(true)
+                
+            }
+        }
+    }, [results])
+    
+    const addInfoHandler = (results) =>{
+        // change the icon to full
+        setFillbookmark(prevFillbookmark => !prevFillbookmark)
+        // getting access to the local storage and adding the current city to the array
+        const getArrayCities = localStorage.getItem("cities")
+        let addCitiesInArray = []
+        if(getArrayCities){
+            addCitiesInArray = JSON.parse(getArrayCities)
+        }
+        const addCity = {name: results.name}
+        addCitiesInArray.push(addCity)
+        localStorage.setItem("cities", JSON.stringify(addCitiesInArray))
+    }
+
+    const removeInfoHandler = (results) =>{
+        // set icon to outlined
+        setFillbookmark(prevFillbookmark => !prevFillbookmark)
+        // getting access to the local storage and filter through to find the city.name and remove it
+        const getArrayCities = localStorage.getItem("cities")
+        let addCitiesInArray = JSON.parse(getArrayCities)
+        const newArray = addCitiesInArray.filter((city) =>{
+            return city.name !== results.name
+        })
+        localStorage.setItem("cities", JSON.stringify(newArray))
+        
+        
+    }
+    return(
+        
+            fillbookmark ?
+            <Bookmark style={{width : "50px", height : "50px" }} onClick={() => removeInfoHandler(results)}/>            
+            :
+            <BookmarkBorderOutlined style={{width : "50px", height : "50px" }} onClick={() => addInfoHandler(results)}/>    
+    )
+}
+
+
+
+
+export default Bookmarks
