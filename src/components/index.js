1bc123df3d12f12eecdfde3f4373a75ee1aa036a
Sana Goyal <sgoyal@unc.edu>
2023-03-20 22:12:58 -0400

    Added activities and reformated index

---
+++ src/components/index.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import foodCategories from "./foodCategories.json";
+
+function activityRecommendation({ weatherCode }) {
+  const [recommendedactivity, setRecommendedactivities] = useState([]);
+
+  useEffect(() => {
+    const activityCategory = chooseactivity(weatherCode);
+
+    //choose 5 random foods from category
+    const randomRecommendations = new Set();
+    while (randomRecommendations.size < 3) {
+      const randIdx = Math.floor(
+        Math.random() * activityCategories[activityCategory].length
+      );
+      randomRecommendations.add(activityCategories[activityCategory][randIdx]);
+    }
+    setRecommendedactivities(Array.from(randomRecommendations));
+  }, [weatherCode]);
+
+  return (
+    <>
+      <div className="activity-recommendation-container">
+        <h2>Perfect activity for this weather!</h2>
+        {recommendedactivity.map((food, index) => (
+          <div className="activity-box" key={index}>
+            <h2 className="activity-name">{activity}</h2>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+function chooseactivity(weatherCode) {
+  if (weatherCode >= 200 && weatherCode <= 232) {
+    return "winter";
+  } else if (weatherCode >= 300 && weatherCode <= 531) {
+    return "spring";
+  } else if (weatherCode >= 600 && weatherCode <= 622) {
+    return "winter";
+  } else if (weatherCode === 800) {
+    return "summer";
+  } else if (weatherCode >= 801 && weatherCode <= 804) {
+    return "fall";
+  } else {
+    return "fall";
+  }
+}
+
+export default activityRecommendation;
