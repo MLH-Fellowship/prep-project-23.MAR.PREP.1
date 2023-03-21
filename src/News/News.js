1d453cb9a4b7e053c30b632ecad27c76a8c68c05
Pradumna <pgautam8601@gmail.com>
2023-03-21 14:11:27 -0400

    Replace News.org API with NewYork Times API

--- src/News/News.js
+++ src/News/News.js
@@ -10,10 +10,10 @@ const News = () => {
     const fetchNews = async () => {
       try{
         const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_NEWS_APIKEY}`
+          `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=puhXmx3sZQZaciMSkqEHwcWOUDJ6dASu`
         )
         if(isComponentMounted){
-          setArticles(response.data.articles);
+          setArticles(response.data.results);
         }
       }catch(error){
         setError(error.response.data.message);
@@ -26,24 +26,31 @@ const News = () => {
       isComponentMounted = false;
     }
   }, []);
-
+ 
   return (
-      articles.length > 0 && !error ? (
-    <div className="container">
-      <h1 className="text-center my-5">Top Headlines</h1>
-      {articles.map((article, index) => (
-        <div className="card mb-3" key={index}>
-          <div className="card-body">
-            <h5 className="card-title">{article.title}</h5>
-            <p className="card-text">{article.description}</p>
-          </div>
+    articles.length > 0 && !error ? (
+  <div className="container">
+    <h1 className="text-center my-5">Top Headlines</h1>
+    {articles.map((article, index) => (
+      <div className="card mb-3" key={index}>
+        <div className="card-body">
+        <div>
+        <img src={article.multimedia[0].url} alt={article.title} className="card-img-top" />
         </div>
-      ))}
-    </div>
-      ) : (
-        error && <div className="error-box"><h1 className="error-text">{error}</h1></div> 
-      )
-  );
+        <div className="card-description">
+          <h5 className="card-title">{article.title}</h5>
+          <p className="card-text">{article.abstract}</p>
+          <a className="card-link" href={article.url} target="_blank" rel="noreferrer">Read More</a>
+        </div>
+        </div>
+      </div>
+    ))}
+    *Data provided by The New York Times
+  </div>
+    ) : (
+      error && <div className="error-box"><h1 className="error-text">{error}</h1></div> 
+    )
+);
 };
 
 export default News;
