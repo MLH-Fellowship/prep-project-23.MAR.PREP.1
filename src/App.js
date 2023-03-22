adf749338e4824cb293df0690aa60684b09f56c2
mukhsadr <mukhsadr@gmail.com>
2023-03-22 15:11:09 -0500

    chore: created Footer folder to store footer comp

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ import Autocomplete from "./components/Autocomplete";
 import SavedPlaces from "./components/Autocomplete/SavedPlaces";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faFacebook, faGithub, faLinkedin, faDiscord,} from "@fortawesome/free-brands-svg-icons";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const [error, setError] = useState(null);
@@ -207,48 +206,6 @@ function App() {
             )}
           </div>
         </div>
-        <footer>
-          <div class="footer">
-            <div class="row">
-              <div class="col-md-6">
-                <p>&copy; 2023 Major League Hacking. All Rights Reserved.</p>
-              </div>
-              <div class="col-md-6">
-              <div className="social-icons">
-          <a
-            href="https://github.com/MLH"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faGithub} size="2x" />
-          </a>
-          <a
-            href="https://www.linkedin.com/school/mlh-fellowship/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faLinkedin} size="2x" />
-          </a>
-          <a
-            href="https://www.facebook.com/MajorLeagueHacking/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faFacebook} size="2x" />
-          </a>
-          <a
-            href="https://discord.mlh.io/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faDiscord} size="2x" />
-          </a>
-        </div>
-              </div>
-            </div>
-          </div>
-        </footer>
-
         <ToastContainer />
         <Suggestion
           weatherType={
@@ -256,6 +213,7 @@ function App() {
           }
           isLoaded={isLoaded}
         ></Suggestion>
+        <Footer></Footer>
       </>
     );
   }
