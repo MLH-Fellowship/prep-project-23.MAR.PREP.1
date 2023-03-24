1421598ca20b636c607a535ba3ed2360d4e6ff99
J0SAL <joyalmeida321@gmail.com>
2023-03-24 15:30:24 +0530

    aligned loader to center

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,16 +2,26 @@ import { MagnifyingGlass } from "react-loader-spinner";
 
 const Loader = () => {
   return (
-    <MagnifyingGlass
-      visible={true}
-      height="80"
-      width="80"
-      ariaLabel="MagnifyingGlass-loading"
-      wrapperStyle={{}}
-      wrapperClass="MagnifyingGlass-wrapper"
-      glassColor="#c0efff"
-      color="#e15b64"
-    />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <MagnifyingGlass
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="MagnifyingGlass-loading"
+        wrapperStyle={{}}
+        wrapperClass="MagnifyingGlass-wrapper"
+        glassColor="#c0efff"
+        color="#e15b64"
+      />
+    </div>
   );
 };
 
